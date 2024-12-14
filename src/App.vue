4024a48c98a35658033938bd0418e29c6c8e585f
Henri Vekuna <cde_1414@yahoo.com>
2024-12-14 19:29:04 +0200

    Header component

--- src/App.vue
+++ src/App.vue
@@ -1,8 +1,10 @@
 <script setup lang="ts">
 import GuidTableContainer from './components/GuidTableContainer.vue';
+import Header from './components/Header.vue';
 
 </script>
 
 <template>
+  <Header />
   <GuidTableContainer />
 </template>
---
+++ src/components/Header.vue
@@ -0,0 +1,11 @@
+<script setup lang="ts">
+import { Menubar } from 'primevue';
+</script>
+
+<template>
+  <Menubar>
+    <template #start>
+        <h1>Guid generator</h1>
+    </template>
+  </Menubar>
+</template>
