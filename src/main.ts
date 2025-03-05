d5b2f1673289b907c41cbd5c8de43283f7cc8edc
Henri Vekuna <cde_1414@yahoo.com>
2025-03-05 06:44:24 +0200

    Move to @primeuix/themes

--- src/main.ts
+++ src/main.ts
@@ -1,6 +1,6 @@
 import { createApp } from 'vue'
 import App from './App.vue'
-import Aura from "@primevue/themes/aura";
+import Aura from "@primeuix/themes/aura";
 import PrimeVue from 'primevue/config';
 import { Button, DataTable } from 'primevue';
 
